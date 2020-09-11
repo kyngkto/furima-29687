c431490d12825ccf46e20b393b39fc17fba8a756
kyngkto <kk.bsk.9013@icloud.com>
2020-09-11 16:39:17 +0900

    トップページの表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-  befor_action :move_to_index, expect: [:index, :show]
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
 end
