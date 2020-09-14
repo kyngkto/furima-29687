c83435d094f2c562e1452466f329787ddba2bab1
kyngkto <kk.bsk.9013@icloud.com>
2020-09-14 21:35:02 +0900

    ユーザー管理機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message:"Password Include both letters and numbers"}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "First name Full-width characters"}
+  validates :last_name, presence: true, format:  {with: /\A[ぁ-んァ-ン一-龥]/, message:"Last name Full-width characters"} 
+  validates :birth_day, presence: true
+  validates :first_name_kana, presence: true, format: { with: /\A[ア-ンー－]+\z/, message:"First name kana Full-width katakana characters"}
+  validates :last_name_kana, presence: true, format: { with: /\A[ア-ンー－]+\z/, message: "Last name kana Full-width katakana characters"}
 end
