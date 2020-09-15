6bbe49e55fac4cf1a6d7caaa9b0b961f2dc99655
kyngkto <kk.bsk.9013@icloud.com>
2020-09-15 19:52:28 +0900

    テストコード３

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
-  validates :email, format: {with: VALID_EMAIL_REGEX}
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]+\z/
   validates :password, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX}
-  #validates :password, format: { with: /\A[a-z\d]{6,100}+\z/i, message:"Include both letters and numbers"}
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
   validates :last_name, presence: true, format:  {with: /\A[ぁ-んァ-ン一-龥]/, message:"Full-width characters"} 
   validates :birth_day, presence: true
-  validates :first_name_kana, presence: true, format: { with: /\A[ア-ンー－]+\z/, message:"Full-width katakana characters"}
-  validates :last_name_kana, presence: true, format: { with: /\A[ア-ンー－]+\z/, message: "Full-width katakana characters"}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message:"Full-width katakana characters"}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
 end
