db1af5e828819a74f776353f9ea642628b770514
kyngkto <kk.bsk.9013@icloud.com>
2020-09-18 10:04:07 +0900

    モデル・テーブル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,10 @@
 class ItemsController < ApplicationController
+
+  #before_action :set_item, only: [:edit,]
   def index
+  end
+
+  def new
   end
 
   def edit
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_083415) do
+ActiveRecord::Schema.define(version: 2020_09_16_015853) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.integer "category_id"
+    t.integer "price"
+    t.integer "condition_id"
+    t.integer "delivery_fee_id"
+    t.integer "area_id"
+    t.integer "date_of_shipment_id"
+    t.bigint "user_id_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id_id"], name: "index_items_on_user_id_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
